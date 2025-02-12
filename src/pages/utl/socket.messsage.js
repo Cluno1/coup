161e88864510013196a1af077a4a62860338ffc7
张连登 <17875477802@163.com>
2025-02-12 22:18:44 +0800

    后端对于websocket的定义的信息

---
+++ src/pages/utl/socket.messsage.js
@@ -0,0 +1,17 @@
+export const serverMessage = {
+  joinRoom: "joinRoom",
+  leaveRoom: "leaveRoom",
+  sendMessage: "sendMessage",
+  setReady: "setReady",
+  setUnready: "setUnready",
+};
+
+export const clientMessage = {
+  playerJoined: "playerJoined",
+  playerLeft: "playerLeft",
+  playersAllReady: "playersAllReady",
+  joinRoomFail: "joinRoomFail",
+  leaveRoomFail: "leaveRoomFail",
+  updatePlayers: "updatePlayers",
+  roomIsFull: "roomIsFull",
+};
